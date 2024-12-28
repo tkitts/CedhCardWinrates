f87da412d11d0e21dddcf72cf12ac52620f9f717
tkitts <tyler.kitts@gmail.com>
2024-12-28 18:25:39 -0500

    fixed vue config

--- CedhCardWinrates/vite.config.js
+++ CedhCardWinrates/vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/CedhCardWinrates/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

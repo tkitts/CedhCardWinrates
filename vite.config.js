422f2569d1117d28fb8a4efd3526ea5261545c21
tkitts <tyler.kitts@gmail.com>
2024-12-29 00:33:20 -0500

    moving docs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/CedhCardWinrates",
+  base:"/CedhCardWinrates/",
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,6 +16,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-
+  build: {
+    outDir: "./docs"
+  }
 })
 

d9ce10f5e35118d9fc7c39b9674705af79d2046d
tkitts <tyler.kitts@gmail.com>
2025-01-05 16:42:32 -0500

    Using online proxy

--- src/components/graphql/apollo.js
+++ src/components/graphql/apollo.js
@@ -4,14 +4,14 @@ import { ApolloLink } from '@apollo/client/core';
 const headLink = new ApolloLink((operation, forward) => {
   operation.setContext({
     headers: {
-      "Access-Control-Allow-Origin": "https://sandbox.embed.apollographql.com",
-      "Access-Control-Allow-Credentials": true
+      //"Access-Control-Allow-Origin": "https://sandbox.embed.apollographql.com",
+      //"Access-Control-Allow-Credentials": true
     }
   })
   return forward(operation);
 })
 
-const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://edhtop16.com/api/graphql'});
+const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://crossorigin.me/https://edhtop16.com/api/graphql'});
 const client = new ApolloClient({
   link: headLink.concat(httpLink),
   cache: new InMemoryCache(),

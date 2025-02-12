69d7680c0ccba0ed05ec89ad87f3359306e27338
tkitts <tyler.kitts@gmail.com>
2025-02-11 19:04:27 -0500

    switched to a suspicious cors proxy

--- src/components/graphql/apollo.js
+++ src/components/graphql/apollo.js
@@ -12,8 +12,8 @@ const headLink = new ApolloLink((operation, forward) => {
 })
 //used to use https://proxy.cors.sh/https://edhtop16.com/api/graphql
 //in dev use /api
-//considering https://thingproxy.freeboard.io/fetch/https://edhtop16.com/api/graphql
-const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://proxy.cors.sh/https://edhtop16.com/api/graphql'});
+//considering https://go.x2u.in/proxy?email=tkitts1721@gmail.com&apiKey=5273dd28&url=
+const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://go.x2u.in/proxy?email=tkitts1721@gmail.com&apiKey=5273dd28&url=https://edhtop16.com/api/graphql'});
 const client = new ApolloClient({
   link: headLink.concat(httpLink),
   cache: new InMemoryCache(),

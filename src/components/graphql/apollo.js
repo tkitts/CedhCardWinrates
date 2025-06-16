225898d1d4780f780cf66f7f28e7503f2650e6f4
tkitts <tyler.kitts@gmail.com>
2025-06-15 22:26:18 -0400

    switch proxies

--- src/components/graphql/apollo.js
+++ src/components/graphql/apollo.js
@@ -13,7 +13,7 @@ const headLink = new ApolloLink((operation, forward) => {
 //used to use https://proxy.cors.sh/https://edhtop16.com/api/graphql
 //in dev use /api
 //considering https://go.x2u.in/proxy?email=tkitts1721@gmail.com&apiKey=5273dd28&url=
-const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://go.x2u.in/proxy?email=tkitts1721@gmail.com&apiKey=5273dd28&url='});
+const httpLink = new HttpLink({ uri: process.env.NODE_ENV === 'development' ? "/api" : 'https://proxy.cors.sh/https://edhtop16.com/api/graphql'});
 const client = new ApolloClient({
   link: headLink.concat(httpLink),
   cache: new InMemoryCache(),
